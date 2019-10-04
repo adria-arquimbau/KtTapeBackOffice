86a99336f2505bd00ed1f123d3bebecf38382073
Adrià Arquimbau Codina <adria.arki@gmail.com>
2019-10-04 09:54:56 +0200

    Results articles max 4 columns

--- ktbo-app/src/components/Results/ResultsCategories/index.js
+++ ktbo-app/src/components/Results/ResultsCategories/index.js
@@ -14,22 +14,23 @@ function ResultsCategories({ searchResult }) {
       },[])
 
     return <>
-    
-        <section className="searchResult">
-        {error && <h4>{error}</h4>}
-            <h4>{message}</h4>
-            {articles && articles.map(item => {
-                const {ref, title, description, img, price, quantity, id} = item
-                return <ul key={id} className="searchResult__article" >
-                    <li className="searchResult__article--param">Ref: {ref}</li>
-                    <li className="searchResult__article--param">{title}</li>
-                    <li className="searchResult__article--param">{description}</li>
-                    <li className="searchResult__article--param"><img alt="" src={img}/></li>
-                    <li className="searchResult__article--param">Price: {price} €</li>
-                    <li className="searchResult__article--param">Stock: {quantity} uds</li>
-                    {logic.isUserLogged() && <CartButton articleId={id} stock={quantity}/>}
-                </ul>
-            })}
+        <section className="searchResultMainContenedor">
+            <section className="searchResult">
+            {error && <h4>{error}</h4>}
+                <h4>{message}</h4>
+                {articles && articles.map(item => {
+                    const {ref, title, description, img, price, quantity, id} = item
+                    return <ul key={id} className="searchResult__article" >
+                        <li className="searchResult__article--param">Ref: {ref}</li>
+                        <li className="searchResult__article--param">{title}</li>
+                        <li className="searchResult__article--param">{description}</li>
+                        <li className="searchResult__article--param"><img alt="" src={img}/></li>
+                        <li className="searchResult__article--param">Price: {price} €</li>
+                        <li className="searchResult__article--param">Stock: {quantity} uds</li>
+                        {logic.isUserLogged() && <CartButton articleId={id} stock={quantity}/>}
+                    </ul>
+                })}
+            </section>
         </section>
     </>
 }
