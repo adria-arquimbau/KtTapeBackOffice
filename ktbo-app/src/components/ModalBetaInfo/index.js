780df777d7690ad4e94390fbd8da3c7a54fcc1ec
Adrià Arquimbau Codina <malarky9@hotmail.es>
2020-01-14 00:43:56 +0100

    up

--- ktbo-app/src/components/ModalBetaInfo/index.js
+++ ktbo-app/src/components/ModalBetaInfo/index.js
@@ -5,7 +5,7 @@ function ModalBetaInfo({message, showModal}) {
     
   return <div className="modal">
   <div className="modal__box">
-    <p>This is a pre-release BETA version. We appreciate your understanding and cooperation.</p>
+    <p>This is a pre-release BETA version. This Beta version does not represent the final quality of the back office. Thank you for your understanding and support.</p>
   <p className="">{message}</p>
     <button onClick={showModal}>Ok</button>
   </div>
