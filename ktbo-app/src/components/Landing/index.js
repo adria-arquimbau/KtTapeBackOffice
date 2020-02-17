cf13588f134592f36ce4baa1e3a72b26fd1154c7
Adrià Arquimbau Codina <malarky9@hotmail.es>
2020-02-17 22:54:08 +0100

    feat: login component funcionando con styles del themplate

--- ktbo-app/src/components/Landing/index.js
+++ ktbo-app/src/components/Landing/index.js
@@ -27,25 +27,9 @@ function Landing() {
   },[])
 
   return  <>
-    <section className="superior">
-      <div className="superior__container">
-        <img className="superior__image" alt="" src="https://kttape.es/wp-content/uploads/2019/02/Logo-sense-fons-dreta-blanc.png" />
-        <section className="superior__conditions">
-          <a className="superior__conditions--privacy"  href="https://kttape.es/privacy-policy/" >Privacy Policy</a>            
-          <a className="superior__conditions--terms"  href="https://kttape.es/terms-of-use/" >Terms of Use</a>
-        </section>
-      </div>
-    </section>
     <section>
       <Login />
     </section>
-    <footer className="footer">
-      <div className="footer__cont">
-        <a className="footer__cont--privacity"  href="https://kttape.es/privacy-policy/" >Privacy Policy</a>            
-        <a className="footer__cont--terms"  href="https://kttape.es/terms-of-use/" >Terms of Use</a>
-      </div>
-      <div className="footer__rights"><a  className="footer__rights--a"  href="https://www.kttape.es">© 2016 KTTAPE EUROPE - Todos los derechos reservados</a></div>
-    </footer>
   </>
 }
 
--- ktbo-app/src/components/Login/index.js
+++ ktbo-app/src/components/Login/index.js
@@ -52,21 +52,20 @@ function Login({ history }) {
         <>
        <div class="sidenav">
          <div class="login-main-text">
-            <h2>Application<br></br> Login Page</h2>
-            <p>Login or register from here to access.</p>
+            <h2>KtTape<br></br> Suppliers Back Office</h2>
          </div>
       </div>
       <div class="main">
          <div class="col-md-6 col-sm-12">
             <div class="login-form">
-               <form>
+               <form onSubmit={handleSubmit}>
                   <div class="form-group">
                      <label>User Name</label>
-                     <input type="text" class="form-control" placeholder="User Name"/>
+                     <input type="text" class="form-control" name="email" placeholder="Email"/>
                   </div>
                   <div class="form-group">
                      <label>Password</label>
-                     <input type="password" class="form-control" placeholder="Password"/>
+                     <input type="password" name="password" class="form-control" placeholder="Password"/>
                   </div>
                   <button type="submit" class="btn btn-black">Login</button>
                   <button type="submit" class="btn btn-secondary">Register</button>
